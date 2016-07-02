5da29c5d45f022784f0e7633fd1015a2a35652c1
Dave King <davidk@tilt.com>
2016-07-02 11:17:17 -0700

    Getting closer

--- QuickReturnDemo/ViewController.swift
+++ QuickReturnDemo/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         tableViewController.scrollDelegate = self
     }
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,20 +32,71 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         self.addChildViewController(tableViewController)
         self.containerView.addSubview(tableView)
 
+        tableView.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
+        tableView.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor).active = true
+
         let headerFrame = self.headerView.frame
         let edgeInsets = UIEdgeInsets(top: headerFrame.origin.y + headerFrame.height, left: 0, bottom: 0, right: 0)
         tableView.contentInset = edgeInsets
         tableView.scrollIndicatorInsets = edgeInsets
     }
 
-    func scrollViewDidScroll(scrollView: UIScrollView) {
-        let scrollOffset = scrollView.contentOffset.y
-        print(scrollView.contentOffset)
+    var stickyScrollPosition: CGFloat = 0.0
+    var previousScrollPosition: CGFloat = 0.0
 
-        if scrollOffset < 0.0 {
-//            var bannerTransform = CATransform3DIdentity
-//            bannerTransform = CATransform3DTranslate(bannerTransform, 0, 500, 0)
-//            bannerView.layer.transform = bannerTransform
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        let bannerHeight = bannerView.frame.height
+        let headerHeight = headerView.frame.height
+
+        let pushDown = bannerHeight + headerHeight
+
+        let scrollOffset = scrollView.contentOffset.y
+
+        let hasScrolledUp = self.previousScrollPosition > scrollOffset
+
+        if !hasScrolledUp {
+            stickyScrollPosition = scrollOffset
         }
+
+        if scrollOffset < bannerHeight - pushDown {
+            print("banner visible")
+
+            let pushUp = -(scrollOffset + pushDown)
+            let bannerTransform = CATransform3DIdentity
+
+            bannerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+            headerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+
+        } else if scrollOffset < (bannerHeight + headerHeight) - pushDown {
+            print("banner invisible, header visible")
+
+            // TODO: sticky scroll position needs to play in here
+
+            let pushUp = -(scrollOffset + pushDown)
+            bannerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+            headerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+
+        } else {
+            print("neither banner inherently visible")
+
+            if scrollOffset < stickyScrollPosition {
+                var headerTransform = CATransform3DIdentity
+                // By default transform the bottom of the header to the top of the view
+                headerTransform = CATransform3DTranslate(headerTransform, 0, -(bannerHeight + headerHeight), 0)
+                // As we scroll up, down
+                headerTransform = CATransform3DTranslate(headerTransform, 0, min(headerHeight, stickyScrollPosition - scrollOffset), 0)
+
+                headerView.layer.transform = headerTransform
+                return
+            }
+
+            // If there's a sticky scroll position
+
+            let pushUp = -(scrollOffset + pushDown)
+            bannerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+            headerView.layer.transform = CATransform3DTranslate(CATransform3DIdentity, 0, pushUp, 0)
+        }
+
+        self.previousScrollPosition = scrollOffset
     }
 }
